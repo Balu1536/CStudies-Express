d9e96000d6717b0df23bc2beb0727d11b9edac6a
Busetty Balasubramanyam <balasubramanyam200517@gmail.com>
2026-01-01 12:32:04 +0000

    2

--- greenfield-center/app.js
+++ greenfield-center/app.js
@@ -7,14 +7,23 @@ app.get('/', (req, res) => {
 res.send('Welcome to Greenfield Community Center!');
 });
 
-app.get('/contacts',(req,res)=>{
-    res.json({
-        email:"balu@gmail.com",
-        phone:"+91 9878767XXXX"
-    });
-});
+
+//case study1. routes to classes and contacts
+const classesRouter = require('./routes/classes');
+const contactRouter =require('./routes/contacts');
+app.use('/classes', classesRouter);
+app.use('/contacts',contactRouter);
+
+
+//case study2. routes to events and contacts
+//const contactRouter = require('./routes/contacts');
+const eventsRouter = require('./routes/events');
+app.use('/contacts',contactRouter);
+app.use('/events', eventsRouter);
+app.use('/public/logo.png',express.static('public/logo.png'));
 
 // Start the server
+
 app.listen(port, () => {
 console.log(`Community Center server running at http://localhost:${port}`);
 });
